fe6df9ff09f3d39fa149811e9daa2c2bd47bb568
gjl <1127879575@qq.com>
2022-04-05 14:45:31 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,4 +7,24 @@ $.ajaxPrefilter(function (options) {
     // 在发起真正的Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
     // console.log(options.url);
+    // 统一为有权限的接口，设置 headers请求头
+
+    if (options.url.indexOf('/my/') >= 0) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 全局统一挂载 complete回调函数
+    options.complete = function (res) {
+        console.log(res);
+        // 在 complete回调函数中，可以使用responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            console.log(1);
+            // 强制清空token
+            localStorage.removeItem('token');
+            // 跳转到登录页
+            location.href = 'login.html';
+        }
+    }
+
 })
